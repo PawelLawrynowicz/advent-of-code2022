ab592cb6bdd2620c41ed163fac7e20c172d802b4
Paweł Ławrynowicz <pawellawrynowicz@protonmail.com>
2022-12-06 00:06:18 +0200

    After finishing task2

--- day1/day1.py
+++ day1/day1.py
@@ -1,14 +1,23 @@
-with open("day1/input", "r") as f:
-    nElves = elfNumber = mostCalories = currentCalories = 0
-
+with open("day1/input") as f:
+    nElves = 0
+    calorieSum = 0
+    elves = dict()
     for line in f:
         if line == '\n':
-            mostCalories = max(currentCalories, mostCalories)
-            if currentCalories == mostCalories:
-                elfNumber = nElves
-            currentCalories = 0
+            elves[nElves] = calorieSum
+            calorieSum = 0
             nElves += 1
             continue
-        currentCalories += int(line[:-1])
+        calorieSum += int(line[:-1])
+    elves = dict(sorted(elves.items(), key=lambda item: item[1], reverse=True))
 
-    print(f"Most calories are carried by Elf #{elfNumber}: {mostCalories}")
+    topThreeCalories = list(elves.values())[:3]
+    topThreeElves = list(elves.keys())[:3]
+
+    print(f"Top three elves with most calories:\n\
+    Elf #{topThreeElves[0]}: {topThreeCalories[0]}\n\
+    Elf #{topThreeElves[1]}: {topThreeCalories[1]}\n\
+    Elf #{topThreeElves[2]}: {topThreeCalories[2]}\n\
+    ")
+
+    print(f"In total they are carrying: {sum(topThreeCalories)}")
---
+++ day1/tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+    Elf #{topThreeElves[0]}: {topThreeCalories[0]}\n\
+    Elf #{topThreeElves[1]}: {topThreeCalories[1]}\n\
+    Elf #{topThreeElves[2]}: {topThreeCalories[2]}\n\
