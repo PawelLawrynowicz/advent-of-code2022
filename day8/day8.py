14110ba67e8b6c68b06db1ec1987b181e44b20db
Paweł Ławrynowicz <pawellawrynowicz@protonmail.com>
2022-12-14 16:46:37 +0200

    day8 task2 completed

--- day8/day8.py
+++ day8/day8.py
@@ -15,13 +15,8 @@ for i in range(len(tree_patch)):
 
 tree_patch = np.array(tree_patch, dtype=int)
 
-
-print(tree_patch)
-
 visible_trees = 0
-
-# visible_trees += 2 * tree_patch_width + 2 * (tree_patch_height - 2)
-
+scenic_score = 0
 
 for i, line in enumerate(tree_patch):
     for j, tree in enumerate(line):
@@ -30,15 +25,24 @@ for i, line in enumerate(tree_patch):
             continue
         else:
             neighbors = list(
-                (tree_patch[i, :j],
-                 tree_patch[i, j + 1:],
-                 tree_patch[i+1:, j],
-                 tree_patch[:i, j])
+                (np.flip(tree_patch[i, :j]),  # left
+                 tree_patch[i, j + 1:],  # right
+                 tree_patch[i+1:, j],  # bottom
+                 np.flip(tree_patch[:i, j]))  # top
             )
             for side in neighbors:
                 if max(side) < tree:
                     visible_trees += 1
                     break
 
+            print(neighbors)
+            side_scores = [0, 0, 0, 0]
+            for side, neighbor_trees in enumerate(neighbors):
+                for neighbor_tree in neighbor_trees:
+                    side_scores[side] += 1
+                    if neighbor_tree >= tree:
+                        break
+            if np.prod(side_scores) > scenic_score:
+                scenic_score = np.prod(side_scores)
 
-print(visible_trees)
+print(scenic_score)
